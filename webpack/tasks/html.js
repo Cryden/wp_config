d4382307ebaa94d34f06c475077d015d80ef2a0a
CRYDEsigN <Cryden_da@mail.ru>
2018-06-20 14:07:56 +0300

    add html and pug tasks

---
+++ webpack/tasks/html.js
@@ -0,0 +1,46 @@
+const fs = require('fs')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let files = []
+let dirFiles = fs.readdirSync(path.join('source/html'))
+
+for (let i = 0; i < dirFiles.length; i++) {
+  if (dirFiles[i].endsWith('.html')) {
+    let keyName = dirFiles[i].substring(0, dirFiles[i].length - '.html'.length)
+    files[keyName] = path.join('source/html', dirFiles[i])
+  }
+}
+
+let HtmlWebpackPluginArray = []
+
+for (var keyName in files) {
+  if (files.hasOwnProperty(keyName)) {
+    let HtmlWebpackPluginValue = new HtmlWebpackPlugin({
+      template: files[keyName],
+      filename: keyName + '.html'
+    })
+    HtmlWebpackPluginArray.push(HtmlWebpackPluginValue)
+  }
+}
+
+module.exports = html => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                attrs: ['img:src']
+              }
+            }
+          ]
+        }
+      ]
+    },
+    plugins: HtmlWebpackPluginArray
+  }
+}
---
+++ webpack/tasks/pug.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+let files = []
+let dirFiles = fs.readdirSync(path.join('source/pug'))
+
+for (let i = 0; i < dirFiles.length; i++) {
+  if (dirFiles[i].endsWith('.pug')) {
+    let keyName = dirFiles[i].substring(0, dirFiles[i].length - '.yml'.length)
+    files[keyName] = path.join('source/pug', dirFiles[i])
+  }
+}
+
+let HtmlWebpackPluginArray = []
+
+for (var keyName in files) {
+  if (files.hasOwnProperty(keyName)) {
+    let HtmlWebpackPluginValue = new HtmlWebpackPlugin({
+      template: files[keyName],
+      filename: keyName + '.html'
+    })
+    HtmlWebpackPluginArray.push(HtmlWebpackPluginValue)
+  }
+}
+
+module.exports = pug => {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.pug$/,
+          use: [
+            {
+              loader: 'html-loader',
+              options: {
+                attrs: ['img:src']
+              }
+            },
+            {
+              loader: 'pug-plain-loader'
+            }
+          ]
+        }
+      ]
+    },
+    plugins: HtmlWebpackPluginArray
+  }
+}
